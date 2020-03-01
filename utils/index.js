21b0a8ff5ff9cfd85cb43697c859f95a5b31b788
Dohun Kim <de.qtner@gmail.com>
2020-03-01 20:20:20 +0800

    Feature: Upgrade REAMDE-editor to add file dir

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,3 @@
-"use strict"
-
 const fs = require("fs")
 const readline = require("readline")
 
@@ -10,69 +8,32 @@ const rl = readline.createInterface({
 
 const filename = "README.md"
 
-const question1 = () => {
-  return new Promise((res, rej) => {
-    rl.question(`Leetcode Question No.? `, ans => {
-      fs.appendFile(filename, `\n|${ans}|`, err => {
-        if (err) return console.log(err)
-      })
-      res()
-    })
-  })
-}
-
-const question2 = () => {
-  return new Promise((res, rej) => {
-    rl.question(`Leetcode Title? `, ans => {
-      let temp = ans.replace(/\s+/g, "-").toLowerCase()
-      let url = `https://leetcode.com/problems/${temp}/`
-      fs.appendFile(filename, `[${ans}](${url})|[JavaScript]()|`, err => {
-        if (err) return console.log(err)
-      })
-      res()
-    })
-  })
-}
-
-const question3 = () => {
-  return new Promise((res, rej) => {
-    rl.question(`Leetcode Difficulty [Easy/Medium/Hard]? `, ans => {
+rl.question(`Leetcode Question No.? `, num => {
+  rl.question(`Leetcode Title? `, title => {
+    let temp = title.replace(/\s+/g, "-").toLowerCase()
+    let url = `https://leetcode.com/problems/${temp}/`
+    rl.question(`Leetcode Difficulty [Easy/Medium/Hard]? `, difficulty => {
       let badge = ""
-
-      if (ans == "Easy") {
+      if (difficulty == "Easy") {
         badge = "![Easy](https://img.shields.io/badge/-Easy-brightgreen)"
-      } else if (ans == "Medium") {
+      } else if (difficulty == "Medium") {
         badge = "![Medium](https://img.shields.io/badge/-Medium-orange)"
-      } else if (ans == "Hard") {
+      } else if (difficulty == "Hard") {
         badge = "![Hard](https://img.shields.io/badge/-Hard-red)"
       }
-
-      fs.appendFile(filename, `${badge}|✔️|`, err => {
-        if (err) return console.log(err)
+      rl.question(`Topic? `, topic => {
+        let newPath = topic.replace(/ /g, "%20")
+        let newFile = `${num}_${temp}.md`
+        fs.appendFile(
+          filename,
+          `\n|${num}|[${title}](${url})|[JavaScript](./${newPath}/${newFile})|${badge}|✔️|${topic}|`,
+          err => {
+            if (err) return console.log(err)
+            console.log("Successfully Added!")
+          }
+        )
+        rl.close()
       })
-      res()
     })
   })
-}
-
-const question4 = () => {
-  return new Promise((res, rej) => {
-    rl.question(`Topic? `, ans => {
-      fs.appendFile(filename, `${ans}|`, err => {
-        if (err) return console.log(err)
-      })
-      res()
-    })
-  })
-}
-
-const compile = async () => {
-  await question1()
-  await question2()
-  await question3()
-  await question4()
-  console.log("Successfully Added!")
-  rl.close()
-}
-
-compile()
+})
